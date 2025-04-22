41291687a6a1b0fed8b94e9ba38ed62ffde6455f
Milos Vasic <i@mvasic.ru>
2025-04-22 21:31:35 +0300

    Small cleanup.

--- scripts/netplan_optimizer.py
+++ scripts/netplan_optimizer.py
@@ -1,4 +1,3 @@
-# Updated config paths for Docker
 CONFIG = {
     'paths': {
         'config': '/etc/netplan/01-netcfg.yaml',
@@ -7,14 +6,12 @@ CONFIG = {
     }
 }
 
-# Load external YAML configs
 def load_configs():
     with open('/etc/netplan_optimizer/notifications.yaml') as f:
         NOTIFICATION_CONFIG.update(yaml.safe_load(f))
     with open('/etc/netplan_optimizer/benchmarks.yaml') as f:
         CONFIG['benchmark'].update(yaml.safe_load(f))
 
-# Modified benchmark commands for container
 def benchmark_interface(interface):
     iperf_cmd = f"timeout {CONFIG['benchmark']['duration']} iperf3 -c {CONFIG['benchmark']['servers'][0]} -p 5202 -J -B {interface}"
     # ... rest of the function
