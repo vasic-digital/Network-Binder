415ac01e58460f3778ec4a395bd21ac88b1ace88
Milos Vasic <i@mvasic.ru>
2025-04-29 18:23:10 +0300

    Fixes.

--- scripts/mptcp-setup.sh
+++ scripts/mptcp-setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-if [ -z "$LAN_GATE_INTERFACE" ]; then
+if [ -z "$LAN_INTERFACE" ]; then
 
-    echo "ERROR: LAN_GATE_INTERFACE is not set"
+    echo "ERROR: LAN_INTERFACE is not set"
+    exit 1
+fi
+
+if ! ip link show "$LAN_INTERFACE" >/dev/null 2>&1; then
+  
+    echo "ERROR: Interface '$LAN_INTERFACE' does not exist"
+    ip link show
     exit 1
 fi
 
@@ -15,14 +22,14 @@ sysctl -w net.mptcp.enabled=1
 [ -f /proc/sys/net/mptcp/mptcp_path_manager ] && \
     sysctl -w net.mptcp.mptcp_path_manager=fullmesh
 
-echo "Starting dnsmasq..."
+echo "Starting dnsmasq on '$LAN_INTERFACE' ..."
 dnsmasq \
     --log-facility=/var/log/dnsmasq.log \
     --no-resolv \
     --server=1.1.1.1 \
     --server=8.8.8.8 \
     --bind-interfaces \
-    --interface="$LAN_GATE_INTERFACE" \
+    --interface="$LAN_INTERFACE" \
     --dhcp-range=192.168.0.100,192.168.0.200,24h \
     --dhcp-option=3,192.168.0.1 \
     --no-daemon &
