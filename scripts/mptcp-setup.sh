f82ba5fa80f551216e35ce3fcf56a6608ff2fac3
Milos Vasic <i@mvasic.ru>
2025-04-30 00:23:26 +0300

    Continued ...

--- scripts/mptcp-setup.sh
+++ scripts/mptcp-setup.sh
@@ -12,8 +12,58 @@ fi
 sysctl -w net.mptcp.enabled=1
 [ -f /proc/sys/net/mptcp/mptcp_path_manager ] && \
     sysctl -w net.mptcp.mptcp_path_manager=fullmesh
+    
+
+WAN_INTERFACES=()
+
+for iface in $(ip link show | grep -E 'eth[0-9]|enp[0-9]s[0-9]' | awk -F': ' '{print $2}' | grep -v "$LAN_INTERFACE"); do
+
+    WAN_INTERFACES+=("$iface")
+done
+
+if [ -z "$WAN_INTERFACES" ]; then
+
+    echo "ERROR: No WAN_INTERFACES"
+    exit 1
+fi
+
+echo "WAN_INTERFACES='$WAN_INTERFACES'"
+
+for iface in "${WAN_INTERFACES[@]}"; do
+    
+    IP_ADDR=$(ip -4 addr show dev "$iface" | grep inet | awk '{print $2}' | cut -d/ -f1)
+    
+    if [ -n "$IP_ADDR" ]; then
+    
+        echo "Creating MPTCP subflow for $iface ($IP_ADDR)"
+        ip mptcp endpoint add "$IP_ADDR" dev "$iface" subflow
+    fi
+done
+
+echo "Current MPTCP endpoints:"
+ip mptcp endpoint show
+
+if ! ip mptcp endpoint show | grep -q subflow; then
+    
+    echo "ERROR: No MPTCP subflows established"
+    echo "Debug info:"
+    echo "- Available interfaces:"; ip link show
+    echo "- IP addresses:"; ip -4 addr show
+    exit 1
+fi
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
+
+if [ -w /etc/sysctl.conf ]; then
+    
+    echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+fi
+
+if [ "$(cat /proc/sys/net/ipv4/ip_forward)" -ne 1 ]; then
+    
+    echo "ERROR: Failed to enable IP forwarding!" >&2
+    exit 1
+fi
 
 iptables -t nat -F
 iptables -t nat -A POSTROUTING -s 192.168.0.0/24 -o eth+ -j MASQUERADE
