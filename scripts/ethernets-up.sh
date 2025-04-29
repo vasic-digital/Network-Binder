48b5d4f2a7c63c08b066d301bca6afc4463745a2
Milos Vasic <i@mvasic.ru>
2025-04-29 11:23:54 +0000

    Bringing up scrip fixes.

--- scripts/ethernets-up.sh
+++ scripts/ethernets-up.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-if ! sudo ip link set up dev $(ip -o link | awk -F': ' '$2 ~ /^e/ {print $2}'); then
+for interface in $(ip -o link show | awk -F': ' '{print $2}' | grep -v "lo"); do
+    
+    STATUS="$(ip -o link show dev $interface | awk '{print $9}')"
 
-    echo "ERROR: Failed to bring up Ethernet interfaces"
-    exit 1
-fi
+    echo "Status for interface '$interface' is '$STATUS'"
+
+    if echo "$STATUS" | grep "DOWN"; then
+        
+        echo "Bringing up $interface..."
+        
+        if ! sudo ip link set "$interface" up; then
+
+            echo "ERROR: Could not brin up the interface '$interface'"
+            exit 1
+        fi
+
+    else
+        
+        echo "$interface is already up"
+    fi
+done
