a7799f3d3a4905290fe726d93e55ba6f2766ab5e
Milos Vasic <i@mvasic.ru>
2025-04-29 15:51:23 +0300

    Testing, fixing and polishing.

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,13 @@ FROM ubuntu:22.04
 RUN apt update && apt install -y \
     iproute2 iperf3 mptcpd \
     dnsmasq iftop ethtool \
-    python3 python3-pip && \
+    python3 python3-pip \
+    netplan.io iptables python3-yaml && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt /app/
+RUN pip3 install -r /app/requirements.txt
 
 COPY scripts/auto-detector.py /app/
 COPY scripts/mptcp-setup.sh /app/
--- scripts/mptcp-setup.sh
+++ scripts/mptcp-setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Enable MPTCP
+# Wait for interfaces to be available
+sleep 5
+
+# Enable MPTCP (skip if kernel doesn't support all features)
 sysctl -w net.mptcp.enabled=1
-sysctl -w net.mptcp.mptcp_path_manager=fullmesh
+[ -f /proc/sys/net/mptcp/mptcp_path_manager ] && \
+    sysctl -w net.mptcp.mptcp_path_manager=fullmesh
 
 # Run auto-detector
 python3 /app/auto-detector.py
@@ -10,16 +14,5 @@ python3 /app/auto-detector.py
 # Apply config
 netplan apply
 
-# Set up NAT
-iptables -t nat -A POSTROUTING -o eth+ -j MASQUERADE
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-# Start DHCP
-dnsmasq \
-    --interface=$LAN_INTERFACE \
-    --dhcp-range=192.168.0.100,192.168.0.200,24h \
-    --dhcp-option=3,192.168.0.1 \
-    --dhcp-option=6,1.1.1.1,8.8.8.8 &
-
-# Keep container running
-while true; do sleep 3600; done
+# Set up NAT (only if iptables is available)
+if command -
