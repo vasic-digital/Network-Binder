115ebe838a3559c9f69d57a6020a05b39fb42f23
Milos Vasic <i@mvasic.ru>
2025-04-29 11:36:42 +0000

    Skipping interfaces.

--- scripts/ethernets-up.sh
+++ scripts/ethernets-up.sh
@@ -4,16 +4,31 @@ for interface in $(ip -o link show | awk -F': ' '{print $2}' | grep -v "lo"); do
     
     STATUS="$(ip -o link show dev $interface | awk '{print $9}')"
 
-    echo "Status for interface '$interface' is '$STATUS'"
+    # echo "Status for interface '$interface' is '$STATUS'"
 
     if echo "$STATUS" | grep "DOWN"; then
         
-        echo "Bringing up $interface..."
-        
-        if ! sudo ip link set "$interface" up; then
+        if echo "$interface" | grep "wlp"; then
 
-            echo "ERROR: Could not brin up the interface '$interface'"
-            exit 1
+            echo "Skipping Wi-Fi interface '$interface'"
+
+        elif echo "$interface" | grep "br-"; then
+
+            echo "Skipping bridge interface '$interface'"
+
+        elif echo "$interface" | grep "docker"; then
+
+            echo "Skipping Docker interface '$interface'"
+
+        else
+
+            echo "Bringing up $interface..."
+        
+            if ! sudo ip link set "$interface" up; then
+
+                echo "ERROR: Could not brin up the interface '$interface'"
+                exit 1
+            fi
         fi
 
     else
